0edec3a68a4bd2b812b5efeb23f646ead446303c
Bravin Murega <tedmurega@gmail.com>
2024-12-16 21:28:57 +0300

    removed the fixed  class

--- src/components/FarmerUI/Dashboard.jsx
+++ src/components/FarmerUI/Dashboard.jsx
@@ -31,7 +31,7 @@ const Dashboard = () => {
       {/* Mobile & Tablet Hamburger Button */}
       <button 
         onClick={toggleSidebar} 
-        className="fixed top-0 left-4 z-50 md:hidden bg-green-600 text-white p-2 rounded-lg"
+        className="top-2 left-4 z-50 md:hidden bg-green-600 text-white p-2 rounded-lg"
       >
         {isSidebarOpen ? <X /> : <Menu />}
       </button>
